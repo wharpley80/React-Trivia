5afbe14f59d3a64afdaac4d875d0f603d74828be
System Administrator <root@Williams-MBP.attlocal.net>
2016-11-19 08:03:17 -0500

    Update Places Red

--- src/reducers/reducer-places.js
+++ src/reducers/reducer-places.js
@@ -39,6 +39,156 @@ export default function() {
 	    option_3: "Capitol Building",
 	    option_4: "Smithsonian Museum",
 	    id: "4"
+	  },
+	  {
+	    answer: "Arches NP",
+	    question: "What's the name of this National Park?",
+	    img: "http://easylistmaker.com/trivia-img/arches-np.jpg",
+	    option_1: "Zion NP",
+	    option_2: "Bryce Canyon NP",
+	    option_3: "Arches NP",
+	    option_4: "Yellowstone NP",
+	    id: "5"
+	  },
+	  {
+	    answer: "Scotland",
+	    question: "The Forth Bridge is located in what country?",
+	    img: "http://easylistmaker.com/trivia-img/cantilever-bridge.jpg",
+	    option_1: "Scotland",
+	    option_2: "Norway",
+	    option_3: "England",
+	    option_4: "Greenland",
+	    id: "6"
+	  },
+	  {
+	    answer: "Brazil",
+	    question: "The iconic Christ the Redeemer Statue is located in what country?",
+	    img: "http://easylistmaker.com/trivia-img/christ-the-redeemer.jpeg",
+	    option_1: "Brazil",
+	    option_2: "Peru",
+	    option_3: "Argentina",
+	    option_4: "Mexico",
+	    id: "7"
+	  },
+	  {
+	    answer: "Wyoming",
+	    question: "The iconic Christ the Redeemer Statue is located in what country?",
+	    img: "http://easylistmaker.com/trivia-img/devils-tower.jpg",
+	    option_1: "Montana",
+	    option_2: "Colorado",
+	    option_3: "South Dakota",
+	    option_4: "Wyoming",
+	    id: "8"
+	  },
+	  {
+	    answer: "Nevada",
+	    question: "Fly Ranch Geyser is located in what state?",
+	    img: "http://easylistmaker.com/trivia-img/fly-ranch-geysers.jpg",
+	    option_1: "California",
+	    option_2: "Oregon",
+	    option_3: "Nevada",
+	    option_4: "Wyoming",
+	    id: "9"
+	  },
+	  {
+	    answer: "Yosemite NP",
+	    question: "Half Dome is located in which National Park?",
+	    img: "http://easylistmaker.com/trivia-img/half-dome.jpg",
+	    option_1: "Yellowstone NP",
+	    option_2: "Yosemite NP",
+	    option_3: "Zion NP",
+	    option_4: "Glacier NP",
+	    id: "10"
+	  },
+	  {
+	    answer: "Amsterdam",
+	    question: "The Heerema Bridge is located in what country?",
+	    img: "http://easylistmaker.com/trivia-img/heerema-bridge.jpg",
+	    option_1: "Amsterdam",
+	    option_2: "England",
+	    option_3: "Scotland",
+	    option_4: "Italy",
+	    id: "11"
+	  },
+	  {
+	    answer: "South Dakota",
+	    question: "Mt Rushmore is located in which state?",
+	    img: "http://easylistmaker.com/trivia-img/mount-rushmore.jpg",
+	    option_1: "Montana",
+	    option_2: "Wyoming",
+	    option_3: "South Dakota",
+	    option_4: "North Dakota",
+	    id: "12"
+	  },
+	  {
+	    answer: "Virginia",
+	    question: "The Pentagon building is located in which state?",
+	    img: "http://easylistmaker.com/trivia-img/pentagon.jpg",
+	    option_1: "Maryland",
+	    option_2: "Virginia",
+	    option_3: "Kentucky",
+	    option_4: "New Jersey",
+	    id: "13"
+	  },
+	  {
+	    answer: "China",
+	    question: "The Shenyang Hun River Ribbon Bridge is located in which country?",
+	    img: "http://easylistmaker.com/trivia-img/shenyang-hun-river-ribbon-bridge.jpg",
+	    option_1: "Japan",
+	    option_2: "North Korea",
+	    option_3: "China",
+	    option_4: "Thailand",
+	    id: "14"
+	  },
+	  {
+	    answer: "England",
+	    question: "The Infinity Bridge is located in which country?",
+	    img: "http://easylistmaker.com/trivia-img/stockton-infinity.jpg",
+	    option_1: "Scotland",
+	    option_2: "Amsterdam",
+	    option_3: "France",
+	    option_4: "England",
+	    id: "15"
+	  },
+	  {
+	    answer: "England",
+	    question: "Stonehenge is located in which country?",
+	    img: "http://easylistmaker.com/trivia-img/stonehenge.jpg",
+	    option_1: "England",
+	    option_2: "Ireland",
+	    option_3: "Greenland",
+	    option_4: "Norway",
+	    id: "16"
+	  },
+	  {
+	    answer: "Arkansas",
+	    question: "Whitaker Point is a lookout point located in which state?",
+	    img: "http://easylistmaker.com/trivia-img/whitaker-point.jpg",
+	    option_1: "Kentucky",
+	    option_2: "Arkansas",
+	    option_3: "Tennessee",
+	    option_4: "Virginia",
+	    id: "17"
+	  },
+	  {
+	    answer: "Yellowstone NP",
+	    question: "What's the name of this National Park?",
+	    img: "http://easylistmaker.com/trivia-img/yellowstone-np.jpg",
+	    option_1: "Glacier NP",
+	    option_2: "Bryce Canyon NP",
+	    option_3: "Yosemite NP",
+	    option_4: "Yellowstone NP",
+	    id: "18"
+	  },
+	  {
+	    answer: "California",
+	    question: "In which state is Yosemite National Park located?",
+	    img: "http://easylistmaker.com/trivia-img/yosemite-valley.jpg",
+	    option_1: "California",
+	    option_2: "Oregon",
+	    option_3: "Wyoming",
+	    option_4: "Arizona",
+	    id: "19"
 	  }
 	];
 }

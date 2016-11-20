76df2a0f503c8053fcfd671bef55e97d29edcc7e
System Administrator <root@Williams-MBP.attlocal.net>
2016-11-20 14:47:44 -0500

    20 Movie Reducer

--- src/reducers/reducer-movie.js
+++ src/reducers/reducer-movie.js
@@ -65,12 +65,120 @@ export default function() {
 	  },
 	  {
 	    answer: "Code Red",
-	    question: "Col. Nathan R. Jessup(Jack Nicholson) ordered this in A Few Good Men?",
+	    question: "Col. Nathan R. Jessup (Jack Nicholson) ordered this in A Few Good Men?",
 	    option_1: "Pizza",
 	    option_2: "Code Red",
 	    option_3: "Nuclear Launch",
 	    option_4: "Bannana Split",
 	    id: "8"
+	  },
+	  {
+	    answer: "Pedro",
+	    question: "Who is Napolean Dynamite's best friend?",
+	    option_1: "Jorge",
+	    option_2: "Paco",
+	    option_3: "Pedro",
+	    option_4: "Ricky",
+	    id: "9"
+	  },
+	  {
+	    answer: "James Earl Jones",
+	    question: "The voice behind Darth Vader?",
+	    option_1: "James Earl Jones",
+	    option_2: "Peter Cullen",
+	    option_3: "Jeremy Irons",
+	    option_4: "Morgan Freeman",
+	    id: "10"
+	  },
+	  {
+	    answer: "Jeremy Irons",
+	    question: "The voice behind Scar from The Lion King?",
+	    option_1: "James Earl Jones",
+	    option_2: "Peter Cullen",
+	    option_3: "Jeremy Irons",
+	    option_4: "Morgan Freeman",
+	    id: "11"
+	  },
+	  {
+	    answer: "Peter Cullen",
+	    question: "The voice behind Optimus Prime in Transformers?",
+	    option_1: "James Earl Jones",
+	    option_2: "Peter Cullen",
+	    option_3: "Jeremy Irons",
+	    option_4: "Leonard Nimoy",
+	    id: "12"
+	  },
+	  {
+	    answer: "Hans",
+	    question: "The villan that Alan Rickman portrays in the original Die Hard?",
+	    option_1: "Albert",
+	    option_2: "Ivan",
+	    option_3: "Satan",
+	    option_4: "Hans",
+	    id: "13"
+	  },
+	  {
+	    answer: "Iowa",
+	    question: "Field of Dreams takes place on a corn field in?",
+	    option_1: "Kansas",
+	    option_2: "Iowa",
+	    option_3: "Nebraska",
+	    option_4: "Ohio",
+	    id: "14"
+	  },
+	  {
+	    answer: "Liv Tyler",
+	    question: "This actress plays Bruce Willis's daughter in Armageddon?",
+	    option_1: "Liv Tyler",
+	    option_2: "Alicia Siverstone",
+	    option_3: "Cameron Diaz",
+	    option_4: "Tea Leoni",
+	    id: "15"
+	  },
+	  {
+	    answer: "Los Angeles",
+	    question: "In what city is the Nakatomi Plaza in the original Die Hard?",
+	    option_1: "New York",
+	    option_2: "Chicago",
+	    option_3: "Los Angeles",
+	    option_4: "San Francisco",
+	    id: "16"
+	  },
+	  {
+	    answer: "President",
+	    question: "What is Morgan Freeman's character in Deep Impact?",
+	    option_1: "Scientist",
+	    option_2: "Detective",
+	    option_3: "President",
+	    option_4: "Professor",
+	    id: "17"
+	  },
+	  {
+	    answer: "Doctor",
+	    question: "What is Harrison Ford's character in The Fugitive?",
+	    option_1: "Scientist",
+	    option_2: "Doctor",
+	    option_3: "President",
+	    option_4: "Archaeologist",
+	    id: "18"
+	  },
+	  {
+	    answer: "50 mph",
+	    question: "In Speed Keanu Reeves must keep the bus above what speed?",
+	    option_1: "45 mph",
+	    option_2: "50 mph",
+	    option_3: "55 mph",
+	    option_4: "60 mph",
+	    id: "19"
+	  },
+	  {
+	    answer: "Dennis Hopper",
+	    question: "This actor plays the villan in the movie Speed?",
+	    option_1: "Dennis Hopper",
+	    option_2: "Alan Rickman",
+	    option_3: "Joe Pesci",
+	    option_4: "Ray Liotta",
+	    id: "20"
 	  }
 	];
 }

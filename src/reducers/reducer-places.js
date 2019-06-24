629cda8e59bf98bad70879b02692587b697e6f8a
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-06-24 06:48:56 -0400

    comment out a places for testing

--- src/reducers/reducer-places.js
+++ src/reducers/reducer-places.js
@@ -410,6 +410,7 @@ export default function() {
 	    option_4: "Rome",
 	    id: "41"
 	  },
+	  /*
 	  {
 	    answer: "Cleveland",
 	    question: "The Rock and Roll Hall of Fame is in what US city?",
@@ -420,6 +421,7 @@ export default function() {
 	    option_4: "San Francisco",
 	    id: "42"
 	  },
+	  */
 	  {
 	    answer: "San Diego",
 	    question: "Name the US city skyline?",

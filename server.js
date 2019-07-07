a8242de84240397e9eff26d8ab1ee02042a677a6
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-07 14:41:55 -0400

    change path to dist

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'));
+	//res.sendFile(path.resolve(__dirname, 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'dist'));
 });
 
 app.listen(port);

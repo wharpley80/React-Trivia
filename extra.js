d578ce63e4377511f352414a68081a6067c24d37
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-08 06:28:43 -0400

    change path in server

--- extra.js
+++ extra.js
@@ -1,9 +1,7 @@
 // Local build for package.json
-//"start": "webpack-dev-server --port 3000 --progress --inline --hot --history-api-fallback"
+"start": "webpack-dev-server --port 3000 --progress --inline --hot --history-api-fallback"
 
 // Production build for package.json
-/*
 "dev": "webpack-dev-server --port 3000 --progress --inline --hot --history-api-fallback",
 "postinstall": "webpack -p --define process.env.NODE_ENV='\"production\"' --progress --colors",
 "start": "node server.js"
-*/
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 const express  = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
@@ -7,8 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'dist'));
 });
 
 app.listen(port);
-*/

ad9871f43d16c1a94d1afd89e841220f2be26836
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-08 09:14:47 -0400

    remove html plugin again

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'index.html'
-		})
-	],
+
 	module: {
 		rules: [
 			{

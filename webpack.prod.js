f1d41f3e38aee5cc30e6d2f10da121898ffc4f9a
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-07 20:41:15 -0400

    changed prod path again

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ var merge = require('webpack-merge');
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle[contentHash].js'
 	}
 });

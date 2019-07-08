eed25cfcdf7275ff0475041c7a292e7329ad1401
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-08 08:23:06 -0400

    add html plugin again

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,11 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html'
+		})
+	],
 	module: {
 		rules: [
 			{
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ var merge = require('webpack-merge');
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist', 'index.html'),
 		filename: 'bundle[contenthash].js'
 	}
 });

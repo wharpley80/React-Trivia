99720755b0a64a50a873797cf4de2ab9984994c4
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 09:31:59 -0400

    remove asset path

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,14 @@
 var distpath = require('path');
 var common = require('./webpack.config');
 var merge = require('webpack-merge');
-const ASSET_PATH = process.env.ASSET_PATH || '/dist';
+//const ASSET_PATH = process.env.ASSET_PATH || '/dist';
 
 module.exports = merge(common, {
 	mode: "production",
 	output: {
 		path: distpath.resolve(__dirname, 'dist'),
-		filename: 'bundle[contenthash].js',
-		publicPath: ASSET_PATH
+		filename: 'bundle[contenthash].js'
+		//publicPath: ASSET_PATH
 	}
   ]
 });

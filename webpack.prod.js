effec409a1ddfa0560b79d3bb57bca588bd3a312
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-08 22:07:39 -0400

    changed bundle path

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ var merge = require('webpack-merge');
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		//path: path.resolve(__dirname, 'dist', 'index.html'),
-		filename: './dist/bundles.js'
+		//path: path.resolve(__dirname, 'dist'),
+		filename: './dist/bundles[contenthash].js'
 	}
 });

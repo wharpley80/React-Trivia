6c2e27455282af4be905fbb91a19cd931e5f7488
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 08:53:13 -0400

    change server path to root

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,11 +6,11 @@ const ASSET_PATH = process.env.ASSET_PATH || '/';
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		path: distpath.resolve(__dirname, 'dist'),
-		filename: 'bundle[contenthash].js',
+		//path: distpath.resolve(__dirname, 'dist'),
+		//filename: 'bundle[contenthash].js',
 
 		//publicPath: ASSET_PATH
-		//path: distpath.resolve(__dirname, '/'),
-		//filename: 'bundle.js'
+		path: distpath.resolve(__dirname, '/'),
+		filename: 'bundle.js'
 	}
 });

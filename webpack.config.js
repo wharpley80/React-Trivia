d19f138b00e467d8b379fd784bcfa73165594e99
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 06:30:47 -0400

    rename dist path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-	
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: './index.html'
+		})
+	],
 	module: {
 		rules: [
 			{
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-var path = require('path');
+var distpath = require('path');
 var common = require('./webpack.config');
 var merge = require('webpack-merge');
 
@@ -7,13 +7,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: './dist/bundle[contenthash].js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			hash: true,
-			template: './dist/index.html'
-		})
-	],
+		path: distpath.resolve(__dirname, 'dist'),
+		filename: 'bundle[contenthash].js'
+	}
 });

ecb07b9b6f6983750c2bc1f0e0b8e4279a2902cf
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 08:35:01 -0400

    create public path

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,15 @@
 var distpath = require('path');
 var common = require('./webpack.config');
 var merge = require('webpack-merge');
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = merge(common, {
 	mode: "production",
 	output: {
 		path: distpath.resolve(__dirname, 'dist'),
-		filename: 'bundle[contenthash].js'
+		filename: 'bundle[contenthash].js',
+
+		publicPath: ASSET_PATH
 		//path: distpath.resolve(__dirname, '/'),
 		//filename: 'bundle.js'
 	}

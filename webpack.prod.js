c7dfb318a5f4fd39cc438bb465f46197f76258fa
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 09:04:23 -0400

    add webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,7 @@
 var distpath = require('path');
 var common = require('./webpack.config');
 var merge = require('webpack-merge');
-//const ASSET_PATH = process.env.ASSET_PATH || '/dist';
-const ASSET_PATH = '/dist';
+const ASSET_PATH = process.env.ASSET_PATH || '/dist';
 
 module.exports = merge(common, {
 	mode: "production",
@@ -10,5 +9,11 @@ module.exports = merge(common, {
 		path: distpath.resolve(__dirname, 'dist'),
 		filename: 'bundle[contenthash].js',
 		publicPath: ASSET_PATH
-	}
+	},
+	plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    })
+  ]
 });

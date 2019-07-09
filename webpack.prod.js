0943ede7d105fdb073c1aaab14655384a4aac0f5
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 07:12:02 -0400

    change webpack intro path

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,12 @@ var distpath = require('path');
 var common = require('./webpack.config');
 var merge = require('webpack-merge');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = merge(common, {
 	mode: "production",
 	output: {
-		path: distpath.resolve(__dirname, 'dist'),
-		filename: 'bundle[contenthash].js'
+		//path: distpath.resolve(__dirname, 'dist'),
+		//filename: 'bundle[contenthash].js'
+		path: distpath.resolve(__dirname, ''),
+		filename: 'bundle.js'
 	}
 });

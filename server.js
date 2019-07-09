867a40e1f5336d1ee41ccc0c3c6ed6931d1f327b
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-09 08:50:50 -0400

    change server path

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express  = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-
+/*
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'dist'));
 });
-
+*/
 app.listen(port);

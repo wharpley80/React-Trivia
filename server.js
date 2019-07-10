f62efde652ccc730a1883d5778bcc7573a5e253a
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-10 09:05:53 -0400

    change server dir

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '/dist'));
+	res.sendFile(path.resolve(__dirname, '/src'));
 });
 
 app.listen(port);
